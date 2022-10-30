9f6d07874703cbb85677c9d3712889255fb3397d
202006359 <202006359@alu.comillas.edu>
2022-10-30 12:51:43 +0100

    modifico carpeta test

---
+++ src/test/resources/CustomerTest.java
@@ -0,0 +1,32 @@
+package test.resources;
+
+import main.domain.Customer;
+
+import static org.junit.Assert.assertEquals;
+import java.io.Serializable;
+
+
+public class CustomerTest implements Serializable{
+	/**
+	 * Clase para verificar el correcto funcionamiento de los metodos de la clase Customer
+	 */
+	private Customer customer;
+	@org.junit.Before
+	public void setUp() throws Exception { //Me creo mi Customer
+		customer=new Customer("Ivan", "IvanPass", "Aventurero");
+	}
+	@org.junit.Test
+	public void getUsuario() { //Verifico usuario
+		assertEquals("Ivan", customer.getUsuario());
+	} //Verifico el usuario
+	@org.junit.Test
+	public void getContrasena() { //Verifico contrasena
+		assertEquals("IvanPass", customer.getContrasena());
+	} //Verifico la contrasena
+	@org.junit.Test
+	public void getPerfil() { //Verifico el perfil
+		assertEquals("Aventurero", customer.getPerfil());
+	} //Verifico el perfil
+
+	
+}
