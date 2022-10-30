d18609b7984e1b7cd6bdcbdff966d98b67f133ce
202006359 <202006359@alu.comillas.edu>
2022-10-30 11:54:09 +0100

    añado las pruebas de Customer

---
+++ src/test/domain/ActividadTest.java
@@ -0,0 +1,15 @@
+package test.domain;
+
+import java.io.Serializable;
+
+public class ActividadTest implements Serializable{
+    /**
+     *
+     */
+
+    private String nombre;
+    private String localizacion;
+
+
+
+}
---
+++ src/test/domain/CustomerTest.java
@@ -0,0 +1,32 @@
+package test.domain;
+
+import main.domain.Customer;
+
+import static org.junit.Assert.assertEquals;
+import java.io.Serializable;
+
+
+public class CustomerTest implements Serializable{
+	/**
+	 * Clase para verificar el correcto funcionamiento de los metodos de la clase Customer
+	 */
+	private Customer customer;
+	@org.junit.Before
+	public void setUp() throws Exception { //Me creo mi Customer
+		customer=new Customer("Ivan", "IvanPass", "Aventurero");
+	}
+	@org.junit.Test
+	public void getUsuario() { //Verifico usuario
+		assertEquals("Ivan", customer.getUsuario());
+	}
+	@org.junit.Test
+	public void getContrasena() { //Verifico contrasena
+		assertEquals("IvanPass", customer.getContrasena());
+	}
+	@org.junit.Test
+	public void getPerfil() { //Verifico el perfil
+		assertEquals("Aventurero", customer.getPerfil());
+	}
+
+	
+}
