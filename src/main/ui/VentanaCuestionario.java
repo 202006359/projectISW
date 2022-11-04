dba399faa1b4220c3b1d686e9354543df736df74
Jaimelloret13 <jaime.lloret2001@gmail.com>
2022-11-04 10:22:53 +0100

    Cuestionario parcheado (cambio switch case y comentarios)

--- src/main/ui/VentanaCuestionario.java
+++ src/main/ui/VentanaCuestionario.java
@@ -5,18 +5,21 @@ import java.awt.event.*;
 import java.util.HashMap;
 import javax.swing.*;
 
+/**
+ * Clase para visualizar en pantalla la ventana del Cuestionario incial
+ */
+
 public class VentanaCuestionario extends JFrame implements ActionListener {
 
     JLabel label;
-    JLabel label1;
-    JRadioButton radioButton[] = new JRadioButton[5]; // tipo de boton, usuario puede ver si esta pulsado o no
-    JLabel labelTexto[] = new JLabel[5];
+    JRadioButton radioButton[] = new JRadioButton[5]; // tipo de boton circular, usuario puede ver si esta pulsado o no
+    JLabel labelTexto[] = new JLabel[5]; //etiqueta para las preguntas y respuestas
     JButton btnSiguiente;
     ButtonGroup bg; // hace que si se pulsa un boton los demás estén bloqueados
     int cuenta;
-    int vecesA, vecesB, vecesC, vecesD;
+    int vecesA, vecesB, vecesC, vecesD; //contador de veces de cada pregunta
     String usuario;
-    String perfil = "Aventurero"; //String perfil; Dejarlo asi cuando Jaime termine la funcionalidad de asignar el perfil
+    String perfil = ""; //Variable de salida para asociar el perfil a 3 categorias de la base de datos
 
     public VentanaCuestionario(String usuario) {
         super("SMART PLAN");
@@ -58,78 +61,78 @@ public class VentanaCuestionario extends JFrame implements ActionListener {
         setSize(600, 350);
     }
 
+    //Metodo mostrar preguntas con sus respuestas y botones para respuesta y boton siguiente
     public void preguntas() {
         radioButton[4].setSelected(true);
-        //Ocio nocturno -> juerguista, Ocio cultural -> aristoteles, Aventuras -> indiana Jones, Gastronomia -> El Comidista, Deportes -> Rafa Nadal, Espiritual -> chill
         if (cuenta == 0) {
             label.setText("PREGUNTA 1: ¿Qué sueles hacer antes y después de salir del trabajo/clase?");
-            labelTexto[0].setText("Me voy a casa esperando a que sea viernes y no hago nada.");//juerguista y aventurero
-            labelTexto[1].setText("Vuelvo a casa lo antes posible para alejarme del estres del curro.");//chill
-            labelTexto[2].setText("Compro el café cerca e intento comer/cenar en algún bar de la zona.");//comidista
-            labelTexto[3].setText("Voy al gimnasio para despejarme después de un día intenso.");//rafa nadal
+            labelTexto[0].setText("Me voy a casa esperando a que sea viernes y no hago nada.");//Ocio Nocturno y Aventuras
+            labelTexto[1].setText("Vuelvo a casa lo antes posible para alejarme del estres del curro.");//Espiritual
+            labelTexto[2].setText("Compro el café cerca e intento comer/cenar en algún bar de la zona.");//Gastronomia
+            labelTexto[3].setText("Voy al gimnasio para despejarme después de un día intenso.");//Deportes
         }
         if (cuenta == 1) {
             label.setText("PREGUNTA 2: Erasmus, vienen tus amigos a tu ciudad, ¿qué haces con ellos?");
-            labelTexto[0].setText("Me los llevo a la discoteca más grande de la ciudad.");//juerguista y aventurero
-            labelTexto[1].setText("Les digo de ir a los mejores museos y conocer cultura.");//aristoteles
-            labelTexto[2].setText("Aprovecho para hacer una comida todos juntos.");//comidista
-            labelTexto[3].setText("Jugamos un partido de selecciones contra italianos.");//rafa nadal
+            labelTexto[0].setText("Me los llevo a la discoteca más grande de la ciudad.");//Ocio Nocturno y Aventuras
+            labelTexto[1].setText("Les digo de ir a los mejores museos y conocer cultura.");//Ocio Cultural
+            labelTexto[2].setText("Aprovecho para hacer una comida todos juntos.");//Gastronomia
+            labelTexto[3].setText("Jugamos un partido de selecciones contra italianos.");//Deportes
         }
         if (cuenta == 2) {
             label.setText("PREGUNTA 3: ¿Qué te habría gustado hacer el dia que cumpliste 18?");
-            labelTexto[0].setText("Salir de fiesta con los amigos que hayan cumplido 18.");//juerguista
-            labelTexto[1].setText("Hacer el examen práctico de conducir y poder hacer viajes con amigos.");//aventurero
-            labelTexto[2].setText("Invitaría a mis amigos a cenar para enseñarles mi restaurante favorito.");//comidista
-            labelTexto[3].setText("Organizar un torneo de padel por parejas con todos mis amigos.");//rafa nadal
+            labelTexto[0].setText("Salir de fiesta con los amigos que hayan cumplido 18.");//Ocio Nocturno
+            labelTexto[1].setText("Hacer el examen práctico de conducir y poder hacer viajes con amigos.");//Aventuras
+            labelTexto[2].setText("Invitaría a mis amigos a cenar para enseñarles mi restaurante favorito.");//Gastronomia
+            labelTexto[3].setText("Organizar un torneo de padel por parejas con todos mis amigos.");//Deportes
         }
         if (cuenta == 3) {
             label.setText("PREGUNTA 4: Cuando acabas los examens del 2ºCuatri, ¿qué sueles hacer?");
-            labelTexto[0].setText("Me voy de fiesta con mis compañeros de clase al sitio de moda.");//juerguista y aventurero
-            labelTexto[1].setText("Intento volver a la cama para dormir una super siesta y liberar tensiones.");//chill
-            labelTexto[2].setText("Ceno en el sitio más lujuso que conozco porque me lo merezco.");//comidista
-            labelTexto[3].setText("Trato de recuperar la forma física que he perdido en examenes yendo al gym.");//rafa nadal
+            labelTexto[0].setText("Me voy de fiesta con mis compañeros de clase al sitio de moda.");//Ocio Nocturno y Aventuras
+            labelTexto[1].setText("Intento volver a la cama para dormir una super siesta y liberar tensiones.");//Espiritual
+            labelTexto[2].setText("Ceno en el sitio más lujuso que conozco porque me lo merezco.");//Gastronomia
+            labelTexto[3].setText("Trato de recuperar la forma física que he perdido en examenes yendo al gym.");//Deportes
         }
         if (cuenta == 4) {
             label.setText("PREGUNTA 5: Te regalan un viaje a Nueva York, ¿qué es lo primero que haces al llegar?");
-            labelTexto[0].setText("Salir a alguna discoteca en la azotea de un rascacielos.");//juerguista y aventurero
-            labelTexto[1].setText("Visito el museo de historia natural.");//aristoteles
-            labelTexto[2].setText("Voy al restaurante con mas estrellas michelin de la ciudad.");//comidista
-            labelTexto[3].setText("Voy a ver un partido de baloncesto de los famososo New York Knicks.");//rafa nadal
+            labelTexto[0].setText("Salir a alguna discoteca en la azotea de un rascacielos.");//Ocio Nocturno y Aventuras
+            labelTexto[1].setText("Visito el museo de historia natural.");//Ocio Cultural
+            labelTexto[2].setText("Voy al restaurante con mas estrellas michelin de la ciudad.");//Gastronomia
+            labelTexto[3].setText("Voy a ver un partido de baloncesto de los famososo New York Knicks.");//Deportes
         }
         if (cuenta == 5) {
             label.setText("PREGUNTA 6: ¿Qué querías ser de pequeño?");
-            labelTexto[0].setText("Quería ser el domador de leones del circo y poder trabajar en un zoo.");//juerguista y aventurero
-            labelTexto[1].setText("Ayudar a los demás en una ONG por el mundo.");//chill
-            labelTexto[2].setText("Chef.");//comidista
-            labelTexto[3].setText("Deportista profesional.");//rafa nadal
+            labelTexto[0].setText("Quería ser el domador de leones del circo y poder trabajar en un zoo.");//Ocio Nocturno y Aventuras
+            labelTexto[1].setText("Ayudar a los demás en una ONG por el mundo.");//Espiritual
+            labelTexto[2].setText("Chef.");//Gastronomia
+            labelTexto[3].setText("Deportista profesional.");//Deportes
         }
         if (cuenta == 6) {
             label.setText("PREGUNTA 7: ¿Cuál de los siguientes planes elegirías para pasar un dia festivo?");
-            labelTexto[0].setText("Pasar el día en el Parque de atracciones.");//aventurero
-            labelTexto[1].setText("Hora Santa con el grupo cristiano HAKUNA.");//chill
-            labelTexto[2].setText("Ir de tapas por Malasaña.");//comidista
-            labelTexto[3].setText("Pasear en bicicleta por El Retiro.");//rafa nadal
+            labelTexto[0].setText("Pasar el día en el Parque de atracciones.");//Aventuras
+            labelTexto[1].setText("Hora Santa con el grupo cristiano HAKUNA.");//Espiritual
+            labelTexto[2].setText("Ir de tapas por Malasaña.");//Gastronomia
+            labelTexto[3].setText("Pasear en bicicleta por El Retiro.");//Deportes
         }
         if (cuenta == 7) {
             label.setText("PREGUNTA 8: ¿Cuál es el rasgo de tu personalidad del que te sientes más orgulloso?");
-            labelTexto[0].setText("El entusiasta, busco pasármelo bien en grupo y ser impetuoso en todo momento.");//juerguista y aventurero
-            labelTexto[1].setText("El sociable, soy atento e intento ser buen compañero.");//chill
-            labelTexto[2].setText("El integrado, me gusta disfrutal de las pequeñas cosas y trato de controlar mi imagen.");//comidista
-            labelTexto[3].setText("El inquieto, activo y animado.");//rafa nadal
+            labelTexto[0].setText("El entusiasta, busco pasármelo bien en grupo y ser impetuoso en todo momento.");//Ocio Nocturno y Aventuras
+            labelTexto[1].setText("El sociable, soy atento e intento ser buen compañero.");//Espiritual
+            labelTexto[2].setText("El integrado, me gusta disfrutal de las pequeñas cosas y trato de controlar mi imagen.");//Gastronomia
+            labelTexto[3].setText("El inquieto, activo y animado.");//Deportes
         }
         if (cuenta == 8) {
             label.setText("PREGUNTA 9: ¿Qué te gustaría hacer si te jubilaras mañana? (te sobra el dinero)");
-            labelTexto[0].setText("Viajar por todo el mundo.");//juerguista y aventurero
-            labelTexto[1].setText("Dedicarme a los demás, ya que tengo mucho tiempo libre.");//chill
-            labelTexto[2].setText("Me apuntaría a mogollón de cursos de cocina y aprender de ese mundillo.");//comidista
-            labelTexto[3].setText("Trataria de seguir haciendo ejercicio para mantenerme en forma y envejecer mejor.");//rafa nadal
+            labelTexto[0].setText("Viajar por todo el mundo.");//jOcio Nocturno y Aventuras
+            labelTexto[1].setText("Dedicarme a los demás, ya que tengo mucho tiempo libre.");//Espiritual
+            labelTexto[2].setText("Me apuntaría a mogollón de cursos de cocina y aprender de ese mundillo.");//Gastronomia
+            labelTexto[3].setText("Trataria de seguir haciendo ejercicio para mantenerme en forma y envejecer mejor.");//Deportes
         }
         if (cuenta == 9) {
             label.setText("PREGUNTA 10: ¿Cuál de estas culturas admiras más?");
-            labelTexto[0].setText("Estadounidense.");//juerguista y aventurero
-            labelTexto[1].setText("Japonesa.");//chill
-            labelTexto[2].setText("Española.");//comidista
-            labelTexto[3].setText("Africana.");//rafa nadal
+            labelTexto[0].setText("Estadounidense.");//Ocio Nocturno y Aventuras
+            labelTexto[1].setText("Japonesa.");//Espiritual
+            labelTexto[2].setText("Española.");//Gastronomia
+            labelTexto[3].setText("Africana.");//Deportes
         }
 
         label.setBounds(30, 40, 450, 20);
@@ -143,65 +146,72 @@ public class VentanaCuestionario extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         if (e.getSource() == radioButton[0]) {
-            vecesA++;
+            vecesA++; //cuenta numero de veces opcion A
         }
         if (e.getSource() == radioButton[1]) {
-            vecesB++;
+            vecesB++; //cuenta numero de veces opcion B
         }
         if (e.getSource() == radioButton[2]) {
-            vecesC++;
+            vecesC++; //cuenta numero de veces opcion C
         }
         if (e.getSource() == radioButton[3]) {
-            vecesD++;
+            vecesD++; //cuenta numero de veces opcion D
         }
         if (e.getSource() == btnSiguiente) {
             cuenta++;
             preguntas();
             if (cuenta == 10) {
                 btnSiguiente.setEnabled(false);
-                if(vecesA == 9)
-                    perfil = "Ocio Cultural;Aventuras;Gastronomia";
-                if(vecesA == 8)
-                    perfil = "Ocio Cultural;Aventuras;Espiritual";
-                if(vecesA == 7)
-                    perfil = "Aventuras;Ocio Cultural;Gastronomia";
-                if(vecesA == 6)
-                    perfil = "Aventuras;Gastronomia;Espiritual";
-                if(vecesA == 5)
-                    perfil = "Ocio Cultural;Deportes;Gastronomia";
-                if(vecesA == 4)
-                    perfil = "Gastronomia;Espiritual;Aventuras";
-                if(vecesA == 3)
-                    perfil = "Gastronomia;Ocio Nocturno;Deportes";
-                if(vecesA == 2)
-                    perfil = "Aventuras;Ocio Cultural;Deportes";
-                if(vecesA == 1)
-                    perfil = "Ocio Nocturno;Espiritual;Gastronomia";
-                if(vecesA == 0)
-                    perfil = "Aventuras;Espiritual;Ocio Cultural";
+                switch(vecesA){
+                    case 9:
+                        perfil = "Ocio Cultural;Aventuras;Gastronomia";
+                        break;
+                    case 8:
+                        perfil = "Ocio Cultural;Aventuras;Espiritual";
+                        break;
+                    case 7:
+                        perfil = "Aventuras;Ocio Cultural;Gastronomia";
+                        break;
+                    case 6:
+                        perfil = "Aventuras;Gastronomia;Espiritual";
+                        break;
+                    case 5:
+                        perfil = "Ocio Cultural;Deportes;Gastronomia";
+                        break;
+                    case 4:
+                        perfil = "Gastronomia;Espiritual;Aventuras";
+                        break;
+                    case 3:
+                        perfil = "Gastronomia;Ocio Nocturno;Deportes";
+                        break;
+                    case 2:
+                        perfil = "Aventuras;Ocio Cultural;Deportes";
+                        break;
+                    case 1:
+                        perfil = "Ocio Nocturno;Espiritual;Gastronomia";
+                        break;
+                    default:
+                        perfil = "Aventuras;Espiritual;Ocio Cultural";
+
+                }
             }
         }
-
-
     }
 
     public void completarCuenta() {
-        Client cliente=new Client();
-        HashMap<String,Object> session=new HashMap<>();
-        String context="/completeAccount";
-        session.put("usuario",usuario);
-        session.put("contrasena",""); //No necesito la contraseña en esta versión
-        session.put("perfil", perfil);
-        cliente.sentMessage(context,session);
-
+            Client cliente=new Client();
+            HashMap<String,Object> session=new HashMap<>();
+            String context="/completeAccount";
+            session.put("usuario",usuario);
+            session.put("contrasena",""); //No necesito la contraseña en esta versión
+            session.put("perfil", perfil);
+            cliente.sentMessage(context,session);
     }
-
 
     private void exit(){
         this.dispose();
         this.setVisible(false);
     }
-
 
 }
 
