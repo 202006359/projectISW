bb0afa38935a9d1654800ab33a07be2aeb935e65
202006359 <202006359@alu.comillas.edu>
2022-11-04 11:12:35 +0100

    actualizo pom

--- src/test/java/domain/ActividadTest.java
+++ src/test/java/domain/ActividadTest.java
@@ -1,20 +1,20 @@
 package domain;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
-public class ActividadTest extends TestCase {
+public class ActividadTest {
     /**
      * Clase para verificar el correcto funcionamiento de los metodos de la clase Actividad
      */
     private Actividad actividad;
     @org.junit.Before
-    public void setUp() throws Exception { //Me creo mi Customer
+    public void setUp() throws Exception { //Me creo mi Actividad
         actividad = new Actividad("Discoteca Liberty", "Ocio Nocturno");
     }
     @org.junit.Test
-    public void getNombre() { //Verifico usuario
+    public void getNombre() { ////Verifico el nombre del plan
         assertEquals("Discoteca Liberty", actividad.getNombre());
-    } //Verifico el nombre del plan
+    }
     @org.junit.Test
     public void getCategoria() { //Verifico la categoria del plan
         assertEquals("Ocio Nocturno", actividad.getCategoria());
