1df1514266ed64af7b6716e59288fa2308e24505
202006359 <202006359@alu.comillas.edu>
2022-11-05 22:53:59 +0100

    Comentarios ivan +

--- src/main/java/controler/ActivitiesControler.java
+++ src/main/java/controler/ActivitiesControler.java
@@ -5,12 +5,12 @@ import domain.Actividad;
 
 import java.util.ArrayList;
 
+/**
+ * Esta clase gestiona las actividades guardadas en la base de datos.
+ */
+
 public class ActivitiesControler {
 
-
-    /**
-     * Esta clase gestiona las actividades guardadas en la base de datos.
-     */
     public static ArrayList<Actividad> getActividades() {
         ActivityDAO.getActividades();
         return ActivityDAO.getActividades();
